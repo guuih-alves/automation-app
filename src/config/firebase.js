bd2cfee7e11b486774770bfed715a7cdc1cb4404
guuih_alves <guilherme_alves_08@hotmail.com>
2025-10-14 23:03:15 -0300

    teste

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB94lLpC6uLWsCEMHlA9v4vjWXzQ36cYLw",
+  authDomain: "vite-contact-7527a.firebaseapp.com",
+  projectId: "vite-contact-7527a",
+  storageBucket: "vite-contact-7527a.firebasestorage.app",
+  messagingSenderId: "346838240767",
+  appId: "1:346838240767:web:c8048da6593814d580abca",
+  measurementId: "G-D0HXNY06WT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
