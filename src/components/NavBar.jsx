3ee0cc0538a2d619ec1320017ba2c844b1be2085
guuih_alves <guilherme_alves_08@hotmail.com>
2025-10-15 21:37:16 -0300

    v-1.0

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,14 @@
 const Navbar = () => {
   return (
     <div className="my-4 flex h-[60px] items-center justify-center gap-8 rounded-lg bg-blue-100 text-xl font-bold">
-      <img src="../public/aut.png" width={60} height={50} />
+      <img src="/aut.png" width={60} height={50} />
       <div className="grid justify-between">
       <h1 className="font-medium">Controle de Estoque</h1>
       <h2>Automação</h2>
    
       </div>
        
-    </div>
-
-
-    
+    </div> 
   );
 };
 
