7840ec861b68dfde226e34b36e92b419bb79799e
guuih-alves <62120624+guuih-alves@users.noreply.github.com>
2025-11-01 19:42:51 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,20 @@
-import Navbar from "./components/NavBar";
-import { FiSearch } from "react-icons/fi";
-import { AiFillPlusCircle } from "react-icons/ai";
-import { useEffect, useState } from "react";
-import { collection, onSnapshot } from "firebase/firestore";
-import { db } from "./config/firebase";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import ContactCard from "./components/ContactCard";
-import AddAndUpdateContact from "./components/AddAndUpdateContact";
-import useDisclouse from "./hooks/useDisclouse";
-import NotFoundContact from "./components/NotFoundContact";
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import './index.css'
 
-const App = () => {
-  const [contacts, setContacts] = useState([]);
+//PAGES &COMPONENTS
+import Login from './login'
+import Material from './Material'
 
-  const { isOpen, onClose, onOpen } = useDisclouse();
-
-  useEffect(() => {
-    const getContacts = async () => {
-      try {
-        const contactsRef = collection(db, "material");
-
-        onSnapshot(contactsRef, (snapshot) => {
-          const contactLists = snapshot.docs.map((doc) => {
-            return {
-              id: doc.id,
-              ...doc.data(),
-            };
-          });
-          setContacts(contactLists);
-          return contactLists;
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getContacts();
-  }, []);
-
-  const filterContacts = (e) => {
-    const value = e.target.value;
-
-    const contactsRef = collection(db, "material");
-
-    onSnapshot(contactsRef, (snapshot) => {
-      const contactLists = snapshot.docs.map((doc) => {
-        return {
-          id: doc.id,
-          ...doc.data(),
-        };
-      });
-
-      const filteredContacts = contactLists.filter((contact) =>
-        contact.Nome.toLowerCase().includes(value.toLowerCase())
-      );
-
-      setContacts(filteredContacts);
-
-      return filteredContacts;
-    });
-  };
+function App() {
 
   return (
-    <>
-      <div className="mx-auto max-w-[370px] px-4">
-        <Navbar />
-        <div className="flex gap-2">
-          <div className="relative flex flex-grow items-center">
-            <FiSearch className="absolute ml-1 text-3xl text-white" />
-            <input
-              onChange={filterContacts}
-              type="text"
-              className=" h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
-              
-            />
-             
-          </div>
-           
+    <Router>
+       <Routes>
+          <Route path="/" element={<Login/>}></Route>
+          <Route path="/material" element={<Material/>}></Route>
+       </Routes>
+    </Router>
+  )
+}
 
-          <AiFillPlusCircle
-            onClick={onOpen}
-            className="cursor-pointer text-5xl text-white"
-          />
-        </div>
-        <div className="mt-4 flex flex-col gap-3">
-          {contacts.length <= 0 ? (
-            <NotFoundContact />
-          ) : (
-            contacts.map((contact) => (
-              <ContactCard key={contact.id} contact={contact} />
-            ))
-          )}
-        </div>
-             <p className="items-center text-white bg-gray-600">Desenvolvido por: Guilherme O Alves</p>
-      </div>
-     
-      <ToastContainer position="bottom-center" />
-      <AddAndUpdateContact onClose={onClose} isOpen={isOpen} />
-    </>
-  );
-};
-
-
-
-export default App;
+export default App
